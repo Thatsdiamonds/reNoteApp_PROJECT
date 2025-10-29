8067b8002a88aea6101b3c4addd15e74af8cf9d4
Galang6sa <galangsur9@gmail.com>
2025-10-29 17:30:56 +0700

    Tampilan login new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,11 @@ dependencies {
 
     implementation(libs.lottie)
     implementation("com.google.android.material:material:1.12.0")
-}
+
 
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
-    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+//    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
-
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/smkth/renotesapp/MainActivity.kt
+++ app/src/main/java/com/smkth/renotesapp/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import android.content.Intent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.smkth.renotesapp.auth.LoginActivity
 
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         auth = FirebaseAuth.getInstance()
 
